5d2bb5306878ee558a25f9f638c132c474e2d4f9
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-09-24 01:20:43 +0530

    fetch coin market data from API and display top 10 coins on home page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
 import { MoveUpRight, Coins } from "lucide-react";
+import { CoinContext } from "../../context/CoinContext";
 
 const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+
+  const currencyHandler = (e) => {
+    switch (e.target.value) {
+      case "usd": {
+        setCurrency({
+          name: "usd",
+          symbol: "$",
+        });
+        break;
+      }
+      case "eur": {
+        setCurrency({
+          name: "eur",
+          symbol: "€",
+        });
+        break;
+      }
+      case "lkr": {
+        setCurrency({
+          name: "lkr",
+          symbol: "Rs",
+        });
+        break;
+      }
+      default: {
+        setCurrency({
+          name: "inr",
+          symbol: "₹",
+        });
+      }
+    }
+  };
+
   return (
     <div className="navbar">
       <div className="logo">
@@ -16,9 +51,10 @@ const Navbar = () => {
         <li>Blog</li>
       </ul>
       <div className="nav-right">
-        <select>
+        <select onChange={currencyHandler}>
           <option value="usd">USD</option>
           <option value="eur">EUR</option>
+          <option value="lkr">LKR</option>
           <option value="inr">INR</option>
         </select>
         <button>
