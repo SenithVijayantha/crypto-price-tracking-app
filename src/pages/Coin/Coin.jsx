3b89d4dc3a1c2b9134c667c30062a94a0d82a769
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-02 12:06:54 +0530

    feat: implement functionality to fetch data from the API

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Coin.css";
+import { useParams } from "react-router";
 
 const Coin = () => {
-  return <div>Coin</div>;
+  const { coinId } = useParams();
+  const [coinData, setCoinData] = useState();
+
+  const fetchCoinData = async () => {
+    const url = `https://api.coingecko.com/api/v3/coins/bitcoin`;
+    const options = {
+      method: "GET",
+      headers: { "x-cg-demo-api-key": import.meta.env.VITE_API_KEY },
+      body: undefined,
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      setAllCoins(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return <div>Coin {coinId}</div>;
 };
 
 export default Coin;
